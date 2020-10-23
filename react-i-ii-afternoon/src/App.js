4a442bd16415eb27576db854ce5400cf57f8cfb3
Jabinator1 <jabinator1@gmail.com>
2020-10-22 18:42:52 -0600

    finished project

---
+++ react-i-ii-afternoon/src/App.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react"
+import "./reset.css"
+import "./App.css"
+import Card from './components/Card'
+import Buttons from './components/Buttons'
+import data from './data/data'
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      data: data,
+      userNumber: 0
+    }
+  }
+
+  increaseNum = () => {
+    if (this.state.userNumber === 24) {
+      this.setState({
+        userNumber: 0
+      })
+    } else {
+      this.setState({
+        userNumber: this.state.userNumber + 1
+      })
+    }
+  }
+
+  decreaseNum = () => {
+    if (this.state.userNumber === 0) {
+      this.setState({
+        userNumber: 24
+      })
+    } else {
+      this.setState({
+        userNumber: this.state.userNumber - 1
+      })
+    }
+  }
+
+  render() {
+    const {data, userNumber} = this.state
+
+    return (
+      <div>
+        <header>
+          <h2>Home</h2>
+        </header>
+        <body>
+          <div className="content">
+            <Card user={data[userNumber]}/>
+            <Buttons increase={this.increaseNum} decrease={this.decreaseNum}/>
+          </div>
+        </body>
+      </div>
+    )
+  }
+}
+
+export default App;
